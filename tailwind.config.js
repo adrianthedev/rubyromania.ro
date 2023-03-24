da124cb3c4bbc05c576e3a63c310fb0131693991
Alex Marinescu <alex.marinescu90@gmail.com>
2023-03-24 11:21:49 +0200

    replace content, add Friendly.rb to current initiatives

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,md,liquid,erb,serb,rb}',
-    './frontend/javascript/**/*.js',
+    "src/**/*",
+    "frontend/**/*"
   ],
   theme: {
     extend: {},
